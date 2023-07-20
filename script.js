718083d5421cb833623fe17141fd2f746a784a3f
HarlanStanbridge <119349@newington.nsw.edu.au>
2023-07-20 12:14:21 +1000

    start of js

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ var checkIn = document.getElementById("checkInDate").value;
 var checkOut = document.getElementById("checkOutDate").value;
 var fName = document.getElementById("firstName").value;
 var surname = document.getElementById("Surname").value;
+let bookedDates = [];
+
+
 
